2b51f145156d1a7324a990aa92b3001b0e7ae8ea
Fiana Vickers <fianablaze@gmail.com>
2021-06-11 18:15:43 +0000

    Add servlet and contact me form

--- portfolio/src/main/java/com/google/sps/servlets/MsgServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MsgServlet.java
@@ -1,18 +1,50 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+
 /** Handles requests sent to the /randMsg URL **/
 @WebServlet("/randMsg")
 public class MsgServlet extends HttpServlet {
 
+//An Array list of video games I have played
+String[] videoGames = new String[]{"Minecraft", "Overwatch", "Pac-man" };
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
+    //response.setContentType("text/html;");
     response.getWriter().println("This is my first Servlet file :D");
+
+    String json = convertToJson(videoGames);
+
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+ 
+  }
+
+  /**
+   * Converts a ServerStats instance into a JSON string using manual String concatentation.
+   */
+  private String convertToJson(String[] videoGameArr) {
+    String json = "{";
+    json += "\"Video Game1\": ";
+    json += "\"" + videoGameArr[0] + "\"";
+    json += ", ";
+    json += "\"Video Game2\": ";
+    json += "\"" + videoGameArr[1] + "\"";
+    json += ", ";
+    json += "\"Video Game3\": ";
+    json += videoGameArr[2];
+    json += "}";
+    return json;
   }
 }
+
--- portfolio/src/main/webapp/script1.js
+++ portfolio/src/main/webapp/script1.js
@@ -1,8 +1,39 @@
 /** Fetches the current date from the server and adds it to the page. */
 async function showRandMsg() {
   const responseFromServer = await fetch('/randMsg');
-  const textFromResponse = await responseFromServer.text();
+  const textFromResponse = await responseFromServer.json();
 
   const msgContainer = document.getElementById('randMsgs-container');
-  msgContainer.innerText = textFromResponse;
+  //msgContainer.innerText = textFromResponse;
+  console.log(textFromResponse.log);
+  console.log(textFromResponse.json);
+
+  msgContainer.innerHTML = '';
+  msgContainer.appendChild(
+      createListElement('Video Game 1: ' + "Minecraft"));
+  msgContainer.appendChild(
+      createListElement('Video Game 2: ' + "Overwatch"));
+  msgContainer.appendChild(
+      createListElement('Video Game 3: ' + "Roblox"));
+  
+}
+
+/** Creates an <li> element containing text. */
+function createListElement(text) {
+  const liElement = document.createElement('li');
+  liElement.innerText = text;
+  return liElement;
+}
+
+function addRandomVideoGame() {
+  const videoGames =
+      ['Minecraft', 'The Sims 4', 'OverWatch',
+       'Pacman'];
+
+  // Pick a random videoGame.
+  const videoGame = videoGames[Math.floor(Math.random() * videoGames.length)];
+
+  // Add it to the page.
+  const gameContainer = document.getElementById('randVid-container');
+  gameContainer.innerText = videoGame;
 }
